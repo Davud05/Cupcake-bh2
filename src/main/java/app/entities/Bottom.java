7d86a069414106bab7748c82f5a64607bd395b5a
Davud05 <Drassuil@gmail.com>
2024-04-05 14:07:31 +0200

    Adjusted bottom and top entities... Created BottomMapper ish now...

--- src/main/java/app/entities/Bottom.java
+++ src/main/java/app/entities/Bottom.java
@@ -1,27 +1,33 @@
 package app.entities;
 
-public class Bottom
-{
+public class Bottom {
     private int bottomId;
     private int bottomPrice;
     private String bottomName;
 
-
+    // Constructor
     public Bottom(int bottomId, int bottomPrice, String bottomName) {
         this.bottomId = bottomId;
         this.bottomPrice = bottomPrice;
         this.bottomName = bottomName;
     }
 
+    // Getter for bottomId
     public int getBottomId() {
         return bottomId;
     }
 
+    // Setter for bottomId
+    public void setBottomId(int bottomId) {
+        this.bottomId = bottomId;
+    }
 
+    // Getter for bottomPrice
     public int getBottomPrice() {
         return bottomPrice;
     }
 
+    // Getter for bottomName
     public String getBottomName() {
         return bottomName;
     }
--- src/main/java/app/entities/Top.java
+++ src/main/java/app/entities/Top.java
@@ -1,11 +1,11 @@
 package app.entities;
 
-public class Top
-{
+public class Top {
     private int topId;
     private int topPrice;
     private String topName;
 
+    // Constructor
     public Top(int topId, int topPrice, String topName) {
         this.topId = topId;
         this.topPrice = topPrice;
@@ -16,18 +16,22 @@ public class Top
         return topId;
     }
 
+    public void setTopId(int topId) {
+        this.topId = topId;
+    }
 
+    // Getter for topPrice
     public int getTopPrice() {
         return topPrice;
     }
 
+    // Getter for topName
     public String getTopName() {
         return topName;
     }
 
     @Override
     public String toString() {
-
         return "Top{" +
                 "topId=" + topId +
                 ", topPrice=" + topPrice +
